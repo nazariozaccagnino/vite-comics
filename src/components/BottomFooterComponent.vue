7dfe4c075cc549c845b81aa4edbf0e5633128b59
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-10 19:15:42 +0200

    bottom footer completed

--- src/components/BottomFooterComponent.vue
+++ src/components/BottomFooterComponent.vue
@@ -4,9 +4,17 @@
             <div id="signup">
                 <p>sign-up now!</p>
             </div>
-            <div>
-                <div id="follow">
+            <div class="d-flex align-items-center">
+                <div id="followus">
                     follow us
+                </div>
+                <div id="socials">
+                    <img src="/img/footer-facebook.png" alt="facebook">
+                    <img src="/img/footer-twitter.png" alt="twitter">
+                    <img src="/img/footer-youtube.png" alt="youtube">
+                    <img src="/img/footer-periscope.png" alt="periscope">
+                    <img src="/img/footer-pinterest.png" alt="pinterest">
+
                 </div>
             </div>
         </div>
@@ -36,6 +44,18 @@
         text-transform: uppercase;
         font-weight: 600;
 
+    }
+}
+#followus{
+    padding: 0px 20px;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 1.3rem;
+    color: $topfooterbackground;
+}
+#socials{
+    img{
+        padding: 0px 10px;
     }
 }
 </style>
