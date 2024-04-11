19543814b6ae24fec46416172f5d9f0775aa4817
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-11 18:10:50 +0200

    fixed the dc logo in footer

--- src/components/MiddleFooterComponent.vue
+++ src/components/MiddleFooterComponent.vue
@@ -2,14 +2,15 @@
     <div id="middlefooter">
         <div class="container">
             <div class="d-flex justify-content-between">
-                <div v-for="(item, index) in actions" :key="index" class="prova">
-                    <div>
-                        <h5>{{ item.title }}</h5>
-                        <ul>
-                            <li v-for="(link, index) in item.links" :key="index"><a href="#">{{ link }}</a></li>
-                        </ul>
+                <div>
+                    <div class="row">
+                        <div class="col" v-for="(item, index) in actions" :key="index">
+                            <h5>{{ item.title }}</h5>
+                            <ul>
+                                <li v-for="(link, index) in item.links" :key="index"><a href="#">{{ link }}</a></li>
+                            </ul>
+                        </div>
                     </div>
-
                 </div>
                 <div id="bigdclogo">
                     <img src="/img/dc-logo-bg.png" alt="">
@@ -85,20 +86,17 @@ export default {
     background-image: url(/img/footer-bg.jpg);
     background-repeat: no-repeat;
     background-size: cover;
-    max-height: 450px;
     color: white;
 
     #bigdclogo {
-        object-fit: contain;
-        border: 1px solid red;
-        height: 400px;
-        object-fit: fill;
+        overflow: hidden;
 
     }
 
     #bigdclogo img {
-        transform: translate(0%, -5%);
-        width: 100%;
+        width: 70%;
+        transform: scale(1.3) translate(50px,0px);
+        object-fit: cover;
     }
 
 }
